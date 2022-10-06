33ac929c6acc63319d0b72e6cae69b45ba74bbbb
hyerriimm <gwj06129@naver.com>
2022-10-06 12:05:33 +0900

    [Create] 채팅방 1차 구현 (버그 수정 전)

--- src/pages/chat/component/Chat.jsx
+++ src/pages/chat/component/Chat.jsx
@@ -1,64 +1,79 @@
 import SockJS from 'sockjs-client'
 import * as StompJs from '@stomp/stompjs'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import { useEffect, useRef, useState, useCallback } from 'react';
 import styled from 'styled-components';
 import { __getChat } from '../../../redux/modules/chat';
 import { useDispatch, useSelector } from 'react-redux';
-// import { useInView } from 'react-intersection-observer';
+import { useInView } from 'react-intersection-observer';
+import { FaArrowCircleUp } from "react-icons/fa";
 
-
+// 채팅 기능 컴포넌트
 const Chat = () => {
+  const navigate = useNavigate();
+   // 게시물 id로 채팅 룸 아이디 연동
   const { id } = useParams();
   const dispatch = useDispatch();
+  // 사용자랑 연결
   const client = useRef({});
-  const chatList = useSelector((state)=>state.chat);
-  // const page = useRef(0);
-  // const [ref, inView] = useInView();
-  // const [hasNextPage, setHasNextPage] = useState(true);
+   // 이전 채팅내용 불러오기
+   const chatList = useSelector((state)=>state.chat);
+   const { isLoading, chatRoomTitle, currentPage, isFirstPage, hasNextPage, totalPage, totalMessage } = useSelector((state)=>state.chat);
 
+   console.log(chatRoomTitle);
+
+   // 인피니티스크롤로 다음페이지 채팅내역 불러오기
+  const page = useRef(0);
+  const [ref, inView] = useInView(); // ref가 화면에 나타나면 inView는 true, 아니면 false를 반환한다.
+  const [stateHasNextPage, setHasNextPage] = useState(hasNextPage);
+  
+  // 실시간 채팅 쌓이는 state
   const [messages, setMessages] = useState([{
     message: "",
     sender: ""
   }]);
+  // 타이핑 치는 input과 연동
   const inputRef = useRef("");
   const [ment, setMent] = useState("");
+  // 이전 채팅 불러와서 출력할 때 닉네임 비교하여 출력여부 결정하는 용도
   let index=0;
+  // 실시간 채팅 닉네임 비교하여 출력여부 결정하는 용도
   let index2= 0;
   
   const scrollRef = useRef(null); //스크롤 하단 고정
-
-
-
   
+  //스크롤 하단 고정
+  useEffect(() => {
+      scrollRef.current?.scrollIntoView()
+  }, [chatList.chatList, messages]);
+ 
+
+  // 랜더링시 이전 채팅내용 불러오는 함수 및 stomp채팅 연결
   useEffect(() => {
     connect();
-    dispatch(__getChat({id})); //,page:page.current
+    dispatch(__getChat({id, page:page.current})); //,page:page.current
     return () =>
-      disconnect();
+    disconnect();
   }, []);
   
 
-  // const fetch = useCallback(()=>{dispatch(__getChat({id, page:page.current})); page.current+=1;}, []);
-  // useEffect(()=>{
-  //   if (inView && hasNextPage) {
-  //     fetch();
-  //   }
-  // }, [fetch, hasNextPage, inView])
+  // 인피니티 스크롤 기능 (다음페이지 데이터 받아옴)
+  const fetch = useCallback(() => { dispatch(__getChat({ id, page: page.current })); page.current += 1; }, []);
+  useEffect(() => {
+    // if (chatList.chatlist?.length < 10 ) {
+    //   return
+    // }
+    if (inView && hasNextPage) {
+      fetch();
+    }
+    // if (inView && Number(currentPage) <= Number(totalPage) ) {
+    //   fetch();
+    // }
+  }, [inView])
 
 
-  //스크롤 하단 고정
-  useEffect(()=>{
-    scrollRef.current?.scrollIntoView({ behavior: "smooth" })
-  }, [messages]);
   
-  useEffect(()=>{
-    scrollRef.current?.scrollIntoView({ behavior: "smooth" })
-  }, []);
-  
-
-
-
+  // 웹소켓 서버와 연결 
   const connect = () => {
     client.current = new StompJs.Client({
       brokerURL: `ws://${process.env.REACT_APP_CHAT_HOST}/ws-stomp/websocket`,
@@ -67,13 +82,15 @@ const Chat = () => {
         RefreshToken: localStorage.getItem('REFRESHTOKEN')
       },
       debug: function (str) {
-        console.log(str);
+        // console.log(str);
       },
       reconnectDelay: 5000,
       heartbeatIncoming: 4000,
       heartbeatOutgoing: 4000,
       onConnect: () => {
         //구독요청
+        // 구독을 통한 채팅방 연결
+        // 처음 입장시 채팅방 서버에 입장한 것으로 인식
         subscribe();
         client.current.publish({
           destination: "/pub/chat/enter",
@@ -94,14 +111,14 @@ const Chat = () => {
     client.current.activate();
   };
 
-
+  // sockJS로 소켓 미지원 브라우저 대응하기
   if (typeof WebSocket !== 'function') {
     client.webSocketFactory = () => {
       return new SockJS(process.env.REACT_APP_CHAT_HOST);
     };
   };
 
-
+   // 구독하기
   const subscribe = () => {
     client.current.subscribe(`/sub/chat/room/${id}`, function (chat) {
       let content = JSON.parse(chat.body);
@@ -113,16 +130,19 @@ const Chat = () => {
     });
   };
 
-
+  // 메세지 보내는 함수
   const submit = () => {
     console.log(JSON.stringify({
+      // 타입 1 = 메세지 보내기
       // type: 1,
       message: inputRef.current.value,
       roomId: id,
     }));
+     // 값이 없으면 전달 x
     if(inputRef.current.value === ""){
       return;
     }
+    // 메세지 보내는 동작
     client.current.publish({
       destination: `/pub/chat/message`,
       headers: {
@@ -136,6 +156,7 @@ const Chat = () => {
         roomId: Number(id),
       }),
     });
+    // 보내고나서 채팅 입력 초기화
     setMent("")
   };
 
@@ -170,22 +191,43 @@ const Chat = () => {
 
   };
 
+  // 엔터로 채팅하기
   const handleKeyPress = e => {
     if (e.key === 'Enter') {
       submit();
     }
   }
 
-  console.log(messages);
+  console.log(chatList);
+  // console.log(messages);
+
+  if (isLoading) {
+    return <Loading>
+      <img alt='로딩중'
+    src='https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif?20151024034921'
+    />
+      </Loading>
+  };
   
   return (
     <>
 
-    <div style={{position:"absolute", top:"600px"}}/> 
-    {/* ref={ref} */}
 
-      <div style={{border:"1px solid black", margin:"2%", height:'90vh'}}>
-        {chatList?.data?.map((chat,i)=>{
+
+    <StDiv>
+        <img
+            alt='뒤로가기'
+            src={process.env.PUBLIC_URL + '/img/backspace.png'}
+            style={{ width: '25px', height: '25px', marginRight: '10px' }}
+            onClick={() => navigate(-1)}
+        />
+        <h3>{chatRoomTitle}</h3>
+    </StDiv>
+      <div style={{margin:"2%", border:"1px solid black", overflow:'scroll', height:'80vh'}}>
+        {/* 인피니티 스크롤 인식 ref */}
+        <div  ref={ref} style={{width:'10px',height:'10px', backgroundColor:"red"}}/> 
+        {/* 이전 채팅내용 불러오기 */}
+        {chatList?.chatList?.map((chat,i)=>{
           if(chat.message === null){
             return;
           }else{
@@ -205,9 +247,9 @@ const Chat = () => {
                 index=i;
                 return(
                   <div key={i}>
-                  <ChatMessage>
+                  {/* <ChatMessage>
                     <MyNick>{chat.sender}</MyNick>
-                  </ChatMessage>
+                  </ChatMessage> */}
                   <ChatMessage style={{ display: "flex", justifyContent: "flex-end" }}>
                     <MyChat>{chat.message}</MyChat>
                   </ChatMessage>
@@ -228,6 +270,9 @@ const Chat = () => {
                 return (
                   <div key={i}>
                   <ChatMessage>
+                    <ProfileImg
+                     style={{backgroundSize:'cover',backgroundImage:`url(${chat.img})`, backgroundPosition: 'center'}}
+                    ></ProfileImg>
                     <NickName>{chat.sender}</NickName>
                   </ChatMessage>
                   <ChatMessage>
@@ -238,6 +283,7 @@ const Chat = () => {
             }
           }
         })}
+         {/* 실시간 채팅 불러오기 */}
         {messages.map((msg, i) => {
           if (msg.sender === "알림") {
             return (
@@ -249,7 +295,7 @@ const Chat = () => {
             return;
           }
           else {
-            if (msg.sender === localStorage.getItem("name")) {
+            if (msg.sender === localStorage.getItem("Id")) {
               if(i>0&&messages[i]?.sender===messages[index2]?.sender){
                 index2=i;
                 return(
@@ -263,9 +309,9 @@ const Chat = () => {
                 index2=i;
                 return (
                   <div key={i}>
-                    <ChatMessage>
+                    {/* <ChatMessage>
                       <MyNick>{msg.sender}</MyNick>
-                    </ChatMessage>
+                    </ChatMessage> */}
                     <ChatMessage style={{ display: "flex", justifyContent: "flex-end" }}>
                       <MyChat>{msg.message}</MyChat>
                     </ChatMessage>
@@ -295,45 +341,114 @@ const Chat = () => {
               }
           }
         })}
-      </div>
-      <div>
+      <div ref={scrollRef}/>
+      </div >
+      <StInputDiv>
         <input ref={inputRef}
           onKeyPress={handleKeyPress}
           value={ment} onChange={(e) => { setMent(e.target.value) }} />
-        <button onClick={() => { submit() }}>전송</button>
-      </div>
-      <div ref={scrollRef}/>
+        <div onClick={() => { submit() }}>
+          <FaArrowCircleUp style={{color: 'grey', display:'flex', justifyContent:'center', fontSize:'30px'}}/>
+        </div>
+      </StInputDiv>
+      {/* 스크롤 하단 고정 */}
     </>
   )
 }
 export default Chat;
 
+const Loading = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 0 auto;
+  width: 80%;
+`;
+
+const StDiv = styled.div`
+  display: flex;
+  width: 85vw;
+  min-width: 320px;
+  max-width: 640px;
+  justify-content: flex-start;
+  align-items: center;
+  /* margin: 0 auto; */
+  margin: 2%;
+  margin-top: 15px;
+  margin-bottom: 10px;
+`
+
 const ChatMessage = styled.div`
 background-color: white;
+display: flex;
 `
 const MyNick = styled.p`
+margin: 10px 0 3px 0;
 background-color: white;
 margin-right:10px;
 text-align:right;
 `
 const MyChat = styled.p`
-background-color:#6D09D1;
+margin: 3px 0;
+background-color:#18A0FB;
 color:white;
 text-align:right;
 width : fit-content;
+max-width: 50%;
 padding:10px;
 border-radius:10px;
+`
+
+const ProfileImg = styled.div`
+  width: 30px;
+  height: 30px;
+  border-radius: 100%;
+  border: 2.5px solid #42A5F5;
 `
 
 const NickName = styled.p`
 background-color: white;
-margin-right:10px;
+margin:10px 0 10px 10px;
 `
 
 const Chatting = styled.p`
-background-color: #EDFFEB;
+margin: 3px 0;
+background-color: #ddd;
 width:fit-content;
+max-width: 50%;
 padding:10px;
 border-radius:10px;
-margin-left:10px;
+margin-left:40px;
+`
+
+const StInputDiv = styled.div`
+  position: fixed; //포인트!
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  line-height: 63px;
+  bottom: 40px; //위치
+  /* left: 40px; //위치 */
+  width: 100vw;
+  height: 30px;
+  input {
+    width: 80%;
+    height: 100%;
+    outline: none;
+    margin-right: 10px;
+    border: 1px solid grey;
+    border-radius: 15px;
+    background-color: rgba( 255, 255, 255, 0.5 );
+    padding-left: 10px;
+  }
+  button {
+    background-color: black;
+    border-radius: 50%;
+    width: 35px;
+    height: 35px;
+    box-sizing: content-box;
+  }
+  div {
+    background-color: white;
+    border-radius: 50%;
+  }
 `
--- src/redux/modules/chat.js
+++ src/redux/modules/chat.js
@@ -6,13 +6,35 @@ import axios from 'axios';
     "/chat/message/{roomId}",
     async (payload, thunkAPI) => {
         try {
-            const data =  await axios.get(process.env.REACT_APP_HOST_PORT+`/chat/message?roomId=${payload.id}`,{
+            const data =  await axios.get(process.env.REACT_APP_HOST_PORT+`/chat/message?roomId=${payload.id}&page=${payload.page}`,{
                 headers: {
                     Authorization: localStorage.getItem("ACCESSTOKEN"),
                     RefreshToken: localStorage.getItem('REFRESHTOKEN')
               }})
+              // console.log(data);
             if(data.data.success===false)
               alert(data.data.error.message);
+            return thunkAPI.fulfillWithValue({data:data.data.data, page:payload.page});
+          } catch (error) {
+            return thunkAPI.rejectWithValue(error);
+          }
+    }
+  );
+
+  // 채팅방 리스트
+  export const __getChatList = createAsyncThunk(
+    "/chat/rooms",
+    async (payload, thunkAPI) => {
+        try {
+            const data =  await axios.get(process.env.REACT_APP_HOST_PORT+`/chat/rooms`,{
+                headers: {
+                    Authorization: localStorage.getItem("ACCESSTOKEN"),
+                    RefreshToken: localStorage.getItem('REFRESHTOKEN')
+              }})
+            if(data.data.success===false) {
+              alert(data.data.error.message);
+              window.location.replace('/');
+            }
             return thunkAPI.fulfillWithValue({data:data.data});
           } catch (error) {
             return thunkAPI.rejectWithValue(error);
@@ -25,38 +47,69 @@ export const chat = createSlice({
     name:"chat",
     initialState: {
         data: [],
+        chatList: [],
+        chatRoomTitle: null,
+        currentPage:0,
+        isFirstPage:true,
+        hasNextPage:false,
+        totalPage:0,
+        totalMessage:0,
         success: false,
         error: null,
         isLoading: false
       },
-    reducers:{
-    },
+    reducers:{},
     // 내부에서 동작하는 함수 외 외부에서 선언해준 함수 동작을 보조하는 기능
-    extraReducers: {
+    extraReducers: (builder) => {
+
       // 대화 내용 불러오기
-        [__getChat.pending]: (state) => {
+      builder
+        .addCase(__getChat.pending, (state) => {
           state.isLoading = true; // 네트워크 요청이 시작되면 로딩상태를 true로 변경합니다.
-        },
-        [__getChat.fulfilled]: (state, action) => {
+        })
+        .addCase(__getChat.fulfilled, (state, action) => {
           state.isLoading = false; // 네트워크 요청이 끝났으니, false로 변경합니다.
-          state.data = action.payload.data.data;
+          // state.data = action.payload.data.data;
 
           // 처음으로 데이터 값 불러올때는 배열에 추가식이 아닌 state값 수정
-        //   if(action.payload.page===0){
-        //       state.data = action.payload.data.data
-        //     // 처음 불러오기때 데이터 변경
-        //   }else{
-        //     // 이후 인피니티 스크롤 시 데이터 앞에 저장
-
-        //   state.data.unshift( ...action.payload.data.data);}
-        },
-        [__getChat.rejected]: (state, action) => {
+            if(action.payload.page===0){
+              // console.log(action.payload);
+              state.chatList = action.payload.data.chatMessageList;
+              state.chatRoomTitle = action.payload.data.chatRoomTitle;
+              state.currentPage = action.payload.data.currentPage;
+              state.isFirstPage = action.payload.data.isFirstPage;
+              state.hasNextPage = action.payload.data.hasNextPage;
+              state.totalPage = action.payload.data.totalPage;
+              state.totalMessage = action.payload.data.totalMessage;
+                // state.data = action.payload.data.data
+              // 처음 불러오기때 데이터 변경
+            }else{
+              // 이후 인피니티 스크롤 시 데이터 앞에 저장
+            state.chatList.unshift( ...action.payload.data.chatMessageList);
+          }
+        })
+        .addCase(__getChat.rejected, (state, action) => {
           state.isLoading = false; // 에러가 발생했지만, 네트워크 요청이 끝났으니, false로 변경합니다.
           state.error = action.payload; // catch 된 error 객체를 state.error에 넣습니다.
-        },
-      },
-      
-    
-})
+        });
+
+      // 채팅방 리스트
+      builder
+        .addCase(__getChatList.pending, (state) => {
+          state.isLoading = true;
+        })
+        .addCase(__getChatList.fulfilled, (state, action) => {
+          state.isLoading = false;
+          state.chatList = action.payload.data.data;
+          // Promise가 fullfilled일 때 dispatch
+        })
+        .addCase(__getChatList.rejected, (state, action) => {
+          state.isLoading = false;
+          state.error = action.payload;
+          // Promise가 rejected일 때 dispatch
+        });
+
+  },
+});
 
 export default chat.reducer;
