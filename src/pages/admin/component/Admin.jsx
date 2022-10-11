4c437c7fb6c526199f3d8a905cc687254ecc51b6
hyerriimm <gwj06129@naver.com>
2022-10-11 18:54:45 +0900

    [Update] 신고내역List 조회하는 모듈, dispatch 수정

--- src/pages/admin/component/Admin.jsx
+++ src/pages/admin/component/Admin.jsx
@@ -1,27 +1,27 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import styled from 'styled-components';
-import { __getReportedMembers, __getReportedPosts, __getReportedComments } from '../../../redux/modules/admin';
+import { __getReportedList } from '../../../redux/modules/admin';
+import BasicTabs from './AdminTap';
 
 const Admin = () => {
   const dispatch = useDispatch();
-  const { ReportedMembers, ReportedPosts, ReportedComments } = useSelector((state) => state.admin);
-
-  // console.log('ReportedMembers', ReportedMembers);
-  // console.log('ReportedPosts', ReportedPosts);
-  // console.log('ReportedComments', ReportedComments);
-  // console.log('렌더링 몇 번');
+  const { ReportedList } = useSelector((state) => state.admin);
+  console.log('ReportedList', ReportedList);
+  // memberList, postList, commentList
 
   useEffect(()=>{
-    dispatch( __getReportedMembers());
-    dispatch( __getReportedPosts());
-    dispatch( __getReportedComments());
-  },[]);
+    dispatch( __getReportedList());
+  },[ReportedList.length]);
 
 
   return (
     <div>
-        신고내역 컴포넌트 Admin.jsx
+      <BasicTabs 
+      postList={ReportedList.postList}
+      commentList={ReportedList.commentList}
+      memberList={ReportedList.memberList}
+      />
     </div>
   );
 };
--- src/redux/modules/admin.js
+++ src/redux/modules/admin.js
@@ -3,12 +3,12 @@ import axios from "axios";
 
 const API_URL = process.env.REACT_APP_HOST_PORT;
 
-// 신고 내역 조회 / 회원
-export const __getReportedMembers = createAsyncThunk(
-  "admin/__getReportedMembers",
+// 신고 내역 조회 / 회원, 게시글 , 댓글
+export const __getReportedList = createAsyncThunk(
+  "admin/__getReportedList",
   async (payload, thunkAPI) => {
     try {
-      const data = await axios.get(`${API_URL}/admin/member`, {
+      const data = await axios.get(`${API_URL}/admin/report/list`, {
         headers: {
           authorization: localStorage.getItem("ACCESSTOKEN"),
           refreshtoken: localStorage.getItem("REFRESHTOKEN"),
@@ -22,52 +22,14 @@ export const __getReportedMembers = createAsyncThunk(
   }
 );
 
-// 신고 내역 조회 / 게시글
-export const __getReportedPosts = createAsyncThunk(
-    "admin/__getReportedPosts",
-    async (payload, thunkAPI) => {
-      try {
-        const data = await axios.get(`${API_URL}/admin/post`, {
-          headers: {
-            authorization: localStorage.getItem("ACCESSTOKEN"),
-            refreshtoken: localStorage.getItem("REFRESHTOKEN"),
-          },
-        });
-        // console.log(data.data.data);
-        return thunkAPI.fulfillWithValue(data.data.data);
-      } catch (error) {
-        return thunkAPI.rejectWithValue(error);
-      }
-    }
-  );
-
-  // 신고 내역 조회 / 댓글
-export const __getReportedComments = createAsyncThunk(
-    "admin/__getReportedComments",
-    async (payload, thunkAPI) => {
-      try {
-        const data = await axios.get(`${API_URL}/admin/comment`, {
-          headers: {
-            authorization: localStorage.getItem("ACCESSTOKEN"),
-            refreshtoken: localStorage.getItem("REFRESHTOKEN"),
-          },
-        });
-         // console.log(data.data.data);
-        return thunkAPI.fulfillWithValue(data.data.data);
-      } catch (error) {
-        return thunkAPI.rejectWithValue(error);
-      }
-    }
-  );
-
-// 신고된 회원 처리 
-export const __refuse = createAsyncThunk(
-  "admin/__refuse",
+// 신고처리 반려 
+export const __withdraw = createAsyncThunk(
+  "admin/__withdraw",
   async (payload, thunkAPI) => {
     try {
       const data = await axios.post(
-        `${API_URL}/admin/member/${payload.reportId}`,
-        { applicationId: payload },
+        `${API_URL}/admin/report/withdraw/${payload}`,
+        { reportId: payload },
         {
           headers: {
             authorization: localStorage.getItem("ACCESSTOKEN"),
@@ -80,7 +42,6 @@ export const __refuse = createAsyncThunk(
         alert(data.data.error.message);
       } else {
         alert(data.data.data);
-        window.location.reload();
       }
       return thunkAPI.fulfillWithValue(data.data.data);
     } catch (error) {
@@ -89,14 +50,14 @@ export const __refuse = createAsyncThunk(
   }
 );
 
-// 신고된 게시글 처리
-export const __accept = createAsyncThunk(
-  "admin/__accept",
+// 신고처리 제재
+export const __execute = createAsyncThunk(
+  "admin/__execute",
   async (payload, thunkAPI) => {
     try {
       const data = await axios.post(
-        `${API_URL}/admin/post/${payload.reportId}`,
-        { applicationId: payload },
+        `${API_URL}/admin/report/execute/${payload.reportId}`,
+        { reportId: payload },
         {
           headers: {
             authorization: localStorage.getItem("ACCESSTOKEN"),
@@ -109,7 +70,6 @@ export const __accept = createAsyncThunk(
         alert(data.data.error.message);
       } else {
         alert(data.data.data);
-        window.location.reload();
       }
       return thunkAPI.fulfillWithValue(data.data.data);
     } catch (error) {
@@ -118,38 +78,11 @@ export const __accept = createAsyncThunk(
   }
 );
 
-// 신고된 댓글 처리
-export const __applycancel = createAsyncThunk(
-  "admin/__applycancel",
-  async (payload, thunkAPI) => {
-    try {
-      const data = await axios.post(
-        `${API_URL}/admin/comment/${payload.reportId}`,
-        { postId: payload },
-        {
-          headers: {
-            authorization: localStorage.getItem("ACCESSTOKEN"),
-            refreshtoken: localStorage.getItem("REFRESHTOKEN"),
-          },
-        }
-      );
-      // console.log(data);
-      if (data.data.success === false) {
-        alert(data.data.error.message);
-      } else {
-        alert(data.data.data);
-        window.location.reload();
-      }
-      return thunkAPI.fulfillWithValue(data.data.data);
-    } catch (error) {
-      return thunkAPI.rejectWithValue(error);
-    }
-  }
-);
 
 export const admin = createSlice({
   name: "admin",
   initialState: {
+    ReportedList:[],
     ReportedMembers: [],
     ReportedPosts: [],
     ReportedComments: [],
@@ -158,44 +91,19 @@ export const admin = createSlice({
   },
   reducers: {},
   extraReducers: (builder) => {
+
     builder
-      .addCase(__getReportedMembers.pending, (state) => {
-        state.isLoading = true;
-      })
-      .addCase(__getReportedMembers.fulfilled, (state, action) => {
-        state.isLoading = false;
-        state.ReportedMembers = action.payload;
-      })
-      .addCase(__getReportedMembers.rejected, (state, action) => {
-        state.isLoading = false;
-        state.error = action.payload;
-      });
-
-      builder
-      .addCase(__getReportedPosts.pending, (state) => {
-        state.isLoading = true;
-      })
-      .addCase(__getReportedPosts.fulfilled, (state, action) => {
-        state.isLoading = false;
-        state.ReportedPosts = action.payload;
-      })
-      .addCase(__getReportedPosts.rejected, (state, action) => {
-        state.isLoading = false;
-        state.error = action.payload;
-      });
-
-      builder
-      .addCase(__getReportedComments.pending, (state) => {
-        state.isLoading = true;
-      })
-      .addCase(__getReportedComments.fulfilled, (state, action) => {
-        state.isLoading = false;
-        state.ReportedComments = action.payload;
-      })
-      .addCase(__getReportedComments.rejected, (state, action) => {
-        state.isLoading = false;
-        state.error = action.payload;
-      });
+    .addCase(__getReportedList.pending, (state) => {
+      state.isLoading = true;
+    })
+    .addCase(__getReportedList.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.ReportedList = action.payload;
+    })
+    .addCase(__getReportedList.rejected, (state, action) => {
+      state.isLoading = false;
+      state.error = action.payload;
+    });
   },
 });
 
