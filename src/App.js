ef23f389f77bb97910ae33e6b11495fb8e71f302
hyerriimm <gwj06129@naver.com>
2022-10-21 22:05:13 +0900

    [Update] +토큰 재발급 로직/ 브라우저 라우터 위치 변경 / 라우터 이름 변경

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
-import Router from "./shared/Router";
+import RouterPg from "./shared/RouterPg";
 import './App.css'
 
 function App() {
   return (
-    <Router />
-    // 시도
+    <RouterPg />
   );
 }
 
