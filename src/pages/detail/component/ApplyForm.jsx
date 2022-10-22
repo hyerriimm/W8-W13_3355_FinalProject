9cf8bf6b8891634f7a4d56af84e85cbb3e25b2e2
gyoung-ha <dlrudgk95@naver.com>
2022-10-23 03:27:11 +0900

    [Update] 지원하기 모임 설명 내용 추가

--- src/pages/detail/component/ApplyForm.jsx
+++ src/pages/detail/component/ApplyForm.jsx
@@ -55,57 +55,114 @@ const ApplyForm = () => {
   return (
     <StContainer>
       <Item2Form onSubmit={onEditHandler}>
-
         <StDiv>
           <img
-            alt='뒤로가기'
+            alt="뒤로가기"
             src={`${process.env.PUBLIC_URL}/img/backspace.png`}
-            style={{ width: '25px', height: '25px', marginRight:'10px' }}
+            style={{ width: "25px", height: "25px", marginRight: "10px" }}
             onClick={() => navigate(-1)}
           />
           <h3>참여 신청하기</h3>
         </StDiv>
 
-        <ApplyTitleDiv>
-          <div> 지원할 모임명</div>
-          <h4>{detail.title}</h4>
-        </ApplyTitleDiv>
-        
-        <StDiv style={{flexDirection:'column', alignItems:"flex-start"}}>
-            <h4 style={{marginLeft:'10px'}}>나를 소개해주세요</h4>
-            <StTextarea
-              cols='27'
-              rows='10'
-                value={content.content}
-                name='content'
-                maxLength={250}
-                onChange={(e) => setContent({content: e.target.value})}
-                placeholder='나를 소개하는 이야기를 입력해주세요. (250자 이내)' 
-            />
+        <Item1>
+          <Img src={detail.postImgUrl} alt="" />
+          <PostContent>
+            <div>
+              <TitleDiv>
+                <h3>{detail.title}</h3>
+              </TitleDiv>
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                }}
+              >
+                <div style={{ display: "flex", alignItems: "center" }}>
+                  <ProfileImg src={detail.memberImgUrl} alt="profile" />
+                  <h4 style={{ width: "150px" }}>{detail.authorNickname}</h4>
+                </div>
+              </div>
+              <br />
+              <strong>모임 설명</strong>
+              <div>{detail.content}</div>
+              <br />
+              <strong>모집 기간</strong>
+              <div>
+                {" "}
+                {detail.startDate} ~ {detail.endDate}
+              </div>
+              <br />
+              <strong>모집 인원</strong>
+              <div>
+                {detail.currentNum}/{detail.maxNum}
+              </div>
+              <br />
+              {/* <div><strong>모임 일자: </strong> {detail.dday}</div> */}
+              <strong>모임 일자</strong>
+              <div>{detail.dday}</div>
+              <br />
+              <strong>모임 장소</strong>
+              <div
+                style={{
+                  color: "#18A0FB",
+                  fontStyle: "oblique",
+                  cursor: "pointer",
+                  width: "fit-content",
+                }}
+                onClick={() => window.open(detail.placeUrl, "_blank")}
+              >
+                {detail.placeName}
+              </div>
+              <div>
+                ( {detail.address}{" "}
+                {detail.detailAddress === "undefined"
+                  ? false
+                  : detail.detailAddress}{" "}
+                )
+              </div>
+            </div>
+          </PostContent>
+        </Item1>
+
+        <StDiv style={{ flexDirection: "column", alignItems: "flex-start" }}>
+          <h4 style={{ margin: "0 auto" }}>나를 소개해주세요</h4>
+          <StTextarea
+            cols="27"
+            rows="10"
+            value={content.content}
+            name="content"
+            maxLength={250}
+            onChange={(e) => setContent({ content: e.target.value })}
+            placeholder="나를 소개하는 이야기를 입력해주세요. (250자 이내)"
+          />
         </StDiv>
 
         <BtnsDiv>
           <StButton
-          type='button' 
-          style={{backgroundColor:'#D9D9D9'}}
-          onClick={() => {
-            if (window.confirm('입력하신 사항은 저장되지 않습니다.\n지원을 취소하시겠습니까?')) {
-              navigate(-1);
-              setContent(initialState);
-            }
-            }}>
+            type="button"
+            style={{ backgroundColor: "#D9D9D9" }}
+            onClick={() => {
+              if (
+                window.confirm(
+                  "입력하신 사항은 저장되지 않습니다.\n지원을 취소하시겠습니까?"
+                )
+              ) {
+                navigate(-1);
+                setContent(initialState);
+              }
+            }}
+          >
             이전으로
           </StButton>
-          <StButton 
-          type='submit'
-          style={{backgroundColor:'#2196F3'}}>
+          <StButton type="submit" style={{ backgroundColor: "#2196F3" }}>
             확인
           </StButton>
         </BtnsDiv>
-
       </Item2Form>
     </StContainer>
-  )
+  );
 };
 
 export default ApplyForm
@@ -120,7 +177,6 @@ const StContainer = styled.div`
 `;
 
 const Item2Form = styled.form`
-  /* background-color: yellow; */
   grid-area: a;
   min-width: 375px;
   display: flex;
@@ -141,8 +197,10 @@ const StTextarea = styled.textarea`
   width: 100%;
   height: 200px;
   margin-top: 10px;
+  padding-top: 10px;
   padding-left: 10px;
   border: transparent;
+  border-top: 1px solid grey;
   border-bottom: 1px solid grey;
   resize: none;
   :focus {
@@ -152,23 +210,6 @@ const StTextarea = styled.textarea`
   }
 `;
 
-const ApplyTitleDiv = styled.div`
-  max-width: 375px;
-  background-color: #d9d9d9;
-  display: flex;
-  flex-direction: column;
-  height: fit-content;
-  padding: 12px;
-  color: black;
-  div {
-    font-size: 15px;
-    font-weight: bold;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    margin-top: 10px;
-  }
-`;
 
 const BtnsDiv = styled.div`
   display: flex;
@@ -188,4 +229,62 @@ const StButton = styled.button`
     filter: brightness(90%);
     box-shadow: 1px 1px 3px 0 #bcd7ff;
   }
+`;
+
+const Item1 = styled.div`
+  /* 720보다 클 때 나오는 화면 */
+  grid-area: a;
+  /* background-color: aliceblue; */
+  display: flex;
+  height: fit-content;
+  /* max-height: 60vh; */
+  width: 100%;
+  max-width: 1240px;
+  max-height: 750px;
+  align-items: center;
+  margin-bottom: 30px;
+  @media only screen and (max-width: 720px) {
+    // 720보다 작을 때 나오는 화면
+    flex-direction: column;
+    max-height: fit-content;
+  }
+`;
+
+const Img = styled.img`
+  /* background-color: orange; */
+  object-fit: contain;
+  width: 50%;
+  /* max-height: 70%;     */
+  max-height: 500px;
+  @media only screen and (max-width: 720px) {
+    width: 100%;
+    max-height: 500px;
+  }
+`;
+
+const PostContent = styled.div`
+  /* background-color: aqua; */
+  box-sizing: border-box;
+  padding-inline: 20px 10px;
+  padding-right: 50px;
+  width: 50%;
+  max-height: 70%;
+  @media only screen and (max-width: 720px) {
+    width: 100%;
+    height: 80%;
+  }
+`;
+
+const TitleDiv = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const ProfileImg = styled.img`
+  width: 30px;
+  height: 30px;
+  border-radius: 100%;
+  margin-right: 20px;
+  border: 2px solid #bcd7ff;
 `;
